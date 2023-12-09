79a130d57f0b1eda82cc7e46a1c18062f5408f42
ALVAROPING1 <alvaroping1@gmail.com>
2023-12-09 19:21:36 +0100

    añadido cambio de menú

--- src/script/reserva.js
+++ src/script/reserva.js
@@ -5,33 +5,54 @@ class Test {
     serialize() {""}
 }
 
+function next_step(prev_step, next_step) {
+    let prev = $(`#${prev_step} > .ProgressIcon`);
+    let next = $(`#${next_step} > .ProgressIcon`);
+    prev.removeClass("Current")
+    next.addClass("Current")
+    prev.addClass("Completed")
+    prev.text("✓")
+}
+
 generate_form(
     "#form-asistentes",
     "Asistentes",
     [
         {name: "Asistentes", valid: Name, placeholder: "Asistentes", hide_name: true},
     ],
-    { name: "Continuar", serialize: Test, callback: () => {} }
+    { name: "Continuar", serialize: Test, callback: function() {
+        $("#form-asistentes").fadeOut("slow", () => { $("#form-fecha").fadeIn(); });
+        next_step("asistentes", "fecha");
+    } }
 )
 
 generate_form(
     "#form-fecha",
     "Fecha",
     [
-        {name: "Pack", valid: Name, placeholder: "Pack *", hide_name: true},
-        {name: "Fecha", valid: Name, placeholder: "Fecha *", hide_name: true},
-        {name: "Hora", valid: Name, placeholder: "Hora *", hide_name: true},
+        {name: "Pack", valid: Name, placeholder: "Pack*", hide_name: true},
+        {name: "Fecha", valid: Name, placeholder: "Fecha*", hide_name: true},
+        {name: "Hora", valid: Name, placeholder: "Hora*", hide_name: true},
     ],
-    { name: "Continuar", serialize: Test, callback: () => {} }
+    { name: "Continuar", serialize: Test, callback: function() {
+        $("#form-fecha").fadeOut("slow", () => { $("#form-informacion-personal").fadeIn(); });
+        next_step("fecha", "informacion-personal");
+    } }
 )
 
 generate_form(
     "#form-informacion-personal",
-    "Fecha",
+    "Información Personal",
     [
-        {name: "Nombre", valid: Name, placeholder: "Nombre Apellido *", hide_name: true},
-        {name: "Email", valid: Email, placeholder: "Correo electrónico* (nombre@dominio.ext)", hide_name: true},
-        {name: "Teléfono", valid: Phone, placeholder: "Teléfono *", hide_name: true},
+        {name: "Nombre", valid: Name, placeholder: "Nombre Apellido*", hide_name: true},
+        {name: "Email", valid: Email, placeholder: "E-mail* (nombre@dominio.ext)", hide_name: true},
+        {name: "Teléfono", valid: Phone, placeholder: "Teléfono* (+34788888888)", hide_name: true},
     ],
-    { name: "Continuar", serialize: Test, callback: () => {} }
+    { name: "Continuar", serialize: Test, callback: function() {
+        $("#form-informacion-personal").fadeOut("slow", () => { $("#form-pago").fadeIn(); });
+        next_step("informacion-personal", "pago");
+    } }
 )
+
+// Hide all steps except the first
+$("form:not(:first-child)").hide();
