1d8871bc01e3dbb0356195d4937439dfcd215ca8
ALVAROPING1 <alvaroping1@gmail.com>
2023-12-13 13:15:33 +0100

    añadida página de contacto

---
+++ src/script/contacto.js
@@ -0,0 +1,30 @@
+class ContactMessaje {
+    constructor(email, subject, messaje) {
+        this.email = email;
+        this.subject = subject;
+        this.messaje = messaje;
+    }
+
+    serialize() {
+        return this.email.value + "-" + this.subject.value + "=\"" + [
+            this.messaje.value.replaceAll("\n", "\\n"),
+        ].join(":") + "\"; SameSite=Strict"
+    }
+}
+
+generate_form(
+    "#form-contacto",
+    "Contacto",
+    [
+        {name: "Email de contacto", valid: Email, placeholder: "ejemplo@dominio.extensión" },
+        {name: "Tema", valid: Text, placeholder: "Mensaje" },
+        {name: "Mensaje", valid: Text, placeholder: "Mensaje", type: "textarea"},
+    ],
+    { name: "Enviar mensaje", serialize: ContactMessaje, callback: () => {} }
+)
+
+$("#faq button").click(function() {
+    $(this).parent().parent().children("p").slideToggle();
+})
+
+$("p.Item").hide();
